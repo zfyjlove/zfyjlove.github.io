e7c752fed6fd257c6705df5ccd13a347a4ad0ff7
zfyjlove <fzhang@se.cuhk.edu.hk>
2023-10-14 21:38:35 +0800

    V0.1

---
+++ js/lovetime.js
@@ -0,0 +1,28 @@
+function lovetime() {
+    window.setTimeout("lovetime()", 1000);
+    var seconds = 1000
+    var minutes = seconds * 60
+    var hours = minutes * 60
+    var days = hours * 24
+    var years = days * 365
+    var today = new Date()
+    var todayYear = today.getFullYear()
+    var todayMonth = today.getMonth()
+    var todayDate = today.getDate()
+    var todayHour = today.getHours()
+    var todayMinute = today.getMinutes()
+    var todaySecond = today.getSeconds()
+    // 修改这里时间即可 我的是2018.11.19
+    var t1 = Date.UTC(2023, 7, 30, 00, 00, 00)
+    var t2 = Date.UTC(todayYear, todayMonth, todayDate, todayHour, todayMinute, todaySecond)
+    var diff = t2 - t1
+    var diffYears = 0
+    var diffDays = Math.floor((diff / days))
+    var diffHours = Math.floor((diff - (diffYears * 365 + diffDays) * days) / hours)
+    var diffMinutes = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours) / minutes)
+    var diffSeconds = Math.floor((diff - (diffYears * 365 + diffDays) * days - diffHours * hours - diffMinutes *
+        minutes) / seconds)
+    document.getElementById("lovetime").innerHTML = "我们认识已经 " + diffDays + "天" +
+        diffHours + "小时" + diffMinutes + "分钟" + diffSeconds + "秒啦"
+}
+lovetime()
